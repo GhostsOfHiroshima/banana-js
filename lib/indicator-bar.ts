38eccd8f3c7c44d41d370b6271c640ff572fac00
蔡貞觀 <leon740727@gmail.com>
2017-03-29 22:21:49 +0800

    將 indicator-bar 獨立出來

---
+++ lib/indicator-bar.ts
@@ -0,0 +1,29 @@
+declare let atom;
+
+function indicatorBar(editor) {
+    let bar = atom.views.getView(editor).component.domNodeValue.querySelector('.indicator-bar');
+    if (! bar) {
+        bar = document.createElement('div');
+        bar.classList.add('indicator-bar');
+        atom.views.getView(editor).component.domNodeValue.appendChild(bar);
+    }
+    return bar;
+}
+
+export function clear(editor) {
+    indicatorBar(editor).innerHTML = '';
+}
+
+export function show(editor, screenPosition) {
+    // 只處理單行 range (因為 identifier 只會有一行)
+    // todo: 用 atom.views.getView(editor).component.domNodeValue.querySelector('.vertical-scrollbar')
+    let row = screenPosition.row;
+    let rowCount = editor.getScreenLineCount();
+    let scrollHeight = atom.views.getView(editor).getScrollHeight();
+    let displayHeight = editor.displayBuffer.height;
+
+    let indicator = document.createElement('div');
+    indicator.classList.add('indicator');
+    indicator.style.top = Math.round((row / rowCount) * displayHeight) + 'px';
+    indicatorBar(editor).appendChild(indicator);
+}
