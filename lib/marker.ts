bd0570941c6ba4fa771883aac52302add225a008
蔡貞觀 <leon740727@gmail.com>
2017-04-01 14:02:07 +0800

    重構

--- lib/marker.ts
+++ lib/marker.ts
@@ -5,12 +5,6 @@ type AtomRange = [number, number];
 export class MarkerManager {
     markerLayerOf = {};
 
-    clear(clsName: string) {
-        if (this.markerLayerOf[clsName]) {
-            this.markerLayerOf[clsName].clear();
-        }
-    }
-
     clearAll() {
         ramda.keys(this.markerLayerOf).forEach(k => this.markerLayerOf[k].clear());
     }
@@ -19,8 +13,10 @@ export class MarkerManager {
         if (! this.markerLayerOf[clsName]) {
             this.markerLayerOf[clsName] = editor.addMarkerLayer();
         }
-        ranges.forEach(range => this.markerLayerOf[clsName].markBufferRange(range, {invalidate: 'touch'}));
-        editor.decorateMarkerLayer(this.markerLayerOf[clsName], {
+        let layer = this.markerLayerOf[clsName];
+        layer.clear();
+        ranges.forEach(range => layer.markBufferRange(range, {invalidate: 'touch'}));
+        editor.decorateMarkerLayer(layer, {
             type: 'highlight',
             class: clsName,
         });
