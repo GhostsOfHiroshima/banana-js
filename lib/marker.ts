e61f2034a37c4495fbc0b8c0856da87763629066
蔡貞觀 <leon740727@gmail.com>
2017-04-01 14:43:22 +0800

    重構: indicatorBar 交由 markerManager 代管

--- lib/marker.ts
+++ lib/marker.ts
@@ -1,23 +1,26 @@
 import * as ramda from 'ramda';
 import {Range} from './base';
+import * as indicatorBar from './indicator-bar';
 
 export class MarkerManager {
     markerLayerOf = {};
 
-    clearAll() {
+    clearAll(editor) {
         ramda.keys(this.markerLayerOf).forEach(k => this.markerLayerOf[k].clear());
+        indicatorBar.clearAll(editor);
     }
 
-    set(editor, clsName: string, ranges: Range[]) {
-        if (! this.markerLayerOf[clsName]) {
-            this.markerLayerOf[clsName] = editor.addMarkerLayer();
+    set(editor, type: string, ranges: Range[]) {
+        if (! this.markerLayerOf[type]) {
+            this.markerLayerOf[type] = editor.addMarkerLayer();
         }
-        let layer = this.markerLayerOf[clsName];
+        let layer = this.markerLayerOf[type];
         layer.clear();
         ranges.forEach(range => layer.markBufferRange(range, {invalidate: 'touch'}));
         editor.decorateMarkerLayer(layer, {
             type: 'highlight',
-            class: clsName,
+            class: type,
         });
+        indicatorBar.set(editor, type, ranges.map(r => r[0]))
     }
 }
