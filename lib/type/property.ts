8aa1ca9d15ffb35c48fc4a22ed50e8a654ee2920
蔡貞觀 <leon740727@gmail.com>
2017-04-05 23:07:39 +0800

    擴充 property.host 的理解範圍

--- lib/type/property.ts
+++ lib/type/property.ts
@@ -5,15 +5,50 @@ import {parent} from './node';
 import {findDefinition} from './identifier';
 import * as commonjs from './module-type/commonjs';
 
+type HostGetter = (node: Identifier) => Optional<Node>;
+const hostGetters: HostGetter[] = [
+    node => {
+        return parent(node)
+        .map(parent => {
+            if (parent.type === 'MemberExpression' && parent.property === node) {
+                return parent.object;
+            } else {
+                return null;
+            }
+        });
+    },
+    node => {
+        return parent(node)
+        .chain(parent)
+        .map(p => {
+            if (p.type === 'ObjectExpression') {
+                return ramda.find(p => p.key === node, p.properties);
+            } else {
+                return null;
+            }
+        });
+    },
+    node => {
+        return parent(node)
+        .chain(parent)
+        .map(p => {
+            if (p.type === 'ClassBody') {
+                return ramda.find(m => m.key === node, p.body)
+            } else {
+                return null;
+            }
+        });
+    }
+]
+
 export function host(node: Identifier): Optional<Node> {
-    return parent(node)
-    .map(parent => {
-        if (parent.type === 'MemberExpression' && parent.property === node) {
-            return parent.object;
+    return hostGetters.reduce((result, getter) => {
+        if (result.is_present()) {
+            return result;
         } else {
-            return null;
+            return getter(node);
         }
-    });
+    }, Optional.empty());
 }
 
 export function isa(node: Identifier): boolean {
